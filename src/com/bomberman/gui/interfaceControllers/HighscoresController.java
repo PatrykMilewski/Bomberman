325d1d6e56f047a665a7312f00caf75442c56911
PatrykMilewski <patryk.milewski@gmail.com>
2017-04-05 23:12:46 +0200

    Added base of Highscores gui.

--- src/com/bomberman/gui/interfaceControllers/HighscoresController.java
+++ src/com/bomberman/gui/interfaceControllers/HighscoresController.java
@@ -1,5 +1,46 @@
 package com.bomberman.gui.interfaceControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
 public class HighscoresController extends MainStageController {
+    @FXML
+    private Pane highscoresPrimaryPane;
+    @FXML
+    private Label top6ScoresLabel;
+    @FXML
+    private Label top1LabelScore;
+    @FXML
+    private Label topLabel;
+    @FXML
+    private Label top2LabelScore;
+    @FXML
+    private Label top4ScoresLabel;
+    @FXML
+    private Label top3LabelScore;
+    @FXML
+    private Label top5ScoresLabel;
+
+    @FXML
+    void resetHighscores() {
+
+    }
+
+    @FXML
+    void mouseClickedLabel() {
+
+    }
+
+    @FXML
+    void mouseEnteredLabel() {
+
+    }
+
+    @FXML
+    void mouseExitedLabel() {
+
+    }
 
 }
