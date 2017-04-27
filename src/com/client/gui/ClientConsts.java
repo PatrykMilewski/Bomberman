ac806f0961207006ee34eae36a344de44e8eab63
Fistelo <radoslaw.mul@gmail.com>
2017-04-27 18:24:08 +0200

    Speed is now incremented after takin' the BONUS BGC

--- src/com/client/gui/ClientConsts.java
+++ src/com/client/gui/ClientConsts.java
@@ -7,7 +7,8 @@ public class ClientConsts {
     public static final long MILIS_TO_EXPLODE = 2400;
     public static final long FIRE_MILIS = 1000;
     public static final long TIME_CUTTER = 10000000;
-    public static final int TIME_BETWEEN_MOVES= 10;
+    public static final int SPEED_INCREMENT = 3;
+    public static final int TIME_BETWEEN_MOVES= 20;
     public static final int TIME_BETWEEN_BOMBS= 20;
     public static final int MAX_RANGE_OF_BOMB = 5;
     public static final int MAX_N_BOMBS = 5;
